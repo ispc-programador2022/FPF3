8b842929d7bc6de87aaa46af259a8ad6a67ecaca
Gonzalo <100076710+gonzalom90@users.noreply.github.com>
2022-11-01 23:01:10 -0300

    Update Indice-felicidad1.py

--- Indice-felicidad1.py
+++ Indice-felicidad1.py
@@ -20,14 +20,21 @@ ranking= []
 
 indice =[]
 
+enlaces = []
+
 var = []
 for tabla in tabla:
   paises.append(tabla.find_all('td')[0].get_text())
   ranking.append(tabla.find_all('td')[1].get_text())
   indice.append(tabla.find_all('td')[2].get_text())
   var.append(tabla.find_all('td')[4].get_text())
+  enlaces.append(tabla.a.get('href'))
 
-df = pd.DataFrame({'paises': paises, 'ranking': ranking, 'indice': indice, 'var': var})
+df = pd.DataFrame({'paises': paises, 'ranking': ranking, 'indice': indice, 'var': var, 'enlace': enlaces})
 
 df.to_csv('Tabla_indicefelicidad.csv', index =False, sep =';')
-df
+#print(df)
+
+
+for cadaPais in enlaces:
+  print('www.pagina.com/seccion/' + cadaPais)
